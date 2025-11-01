08c9506506cac71b0a2e620b86fcde3d69353422
SocketSomeone <socket.someone@gmail.com>
2025-11-01 13:46:53 +0300

    chore: update eslint commands

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,12 @@
-import { FlatCompat } from '@eslint/eslintrc'
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
-const compat = new FlatCompat({
-	baseDirectory: import.meta.dirname,
-})
-
-const eslintConfig = [
-	...compat.extends('next/core-web-vitals', 'next/typescript'),
-	{
-		rules: {
-			'@typescript-eslint/no-explicit-any': 'off',
-		}
-	},
-]
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+    }
+}, {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}]
 
 export default eslintConfig
